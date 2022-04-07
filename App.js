8f219f2069632934110a65049541b2a1d3d923a5
Patricia Mazuelo <patriciamazuelo@gmail.com>
2022-04-07 06:42:58 +0200

    Creand APP con expo y primera tipografía

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { useState, useEffect } from "react";
+import * as Font from "expo-font";
+import React from "react";
 
 export default function App() {
+  const [loadedFonts, setLoadedFonts] = useState(false);
+
+  useEffect(() => {
+    if (!loadedFonts) {
+      loadFonts();
+    }
+  });
+
+  const loadFonts = async () => {
+    await Font.loadAsync({
+      "indie-flower": require("./assets/fonts/IndieFlower-Regular.ttf"),
+    });
+
+    setLoadedFonts(true);
+  };
+
+  if (!loadedFonts) {
+    return <View />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>LOS PINGÜINITOS</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +36,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ffcce5",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 40,
+    fontFamily: "indie-flower",
   },
 });
